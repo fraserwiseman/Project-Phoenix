db765d594b03b8a9668711c15c3f6a5202929978
fwiseman <f.wiseman1994@gmail.com>
2019-11-22 10:24:21 +0100

    2. Transfer Tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var FireToken = artifacts.require("./FireToken.sol"); // first 'var' was 'const'
 
 module.exports = function(deployer) {
-  deployer.deploy(FireToken);
+  deployer.deploy(FireToken, 1000000);
 };
